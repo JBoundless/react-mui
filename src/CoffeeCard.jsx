d4175d6986444c3fd8d656610144b7e2a6932f04
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-05 18:01:08 -0700

    Initial commit

---
+++ src/CoffeeCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+
+const CoffeeCard = (props) => {
+  const { avatarUrl, title, subtitle, description, imageUrl } = props;
+  return (
+    <Card>
+      <CardHeader
+        avatar={<Avatar src={avatarUrl} />}
+        action={
+          <IconButton aria-label="settings">
+            <ShareIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader={subtitle}
+      />
+      <CardMedia style={{ height: "150px" }} image={imageUrl} />
+      <CardContent>
+        <Typography variant="body2" component="p">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">BUY NOW</Button>
+        <Button size="small">OFFER</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CoffeeCard;
